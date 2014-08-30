adb17f19800057d53845be0e0f8c9f8ae0cd960c
Mikkel Holck Madsen <mhm@oxygensoftware.dk>
2014-08-30 22:55:01 +0200

    Added a SearchResultItem, that represents the searchindex

---
+++ MovieShop/MovieShop.BusinessLayer/Search/MovieSearchResultItem.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.SearchTypes;
+using Sitecore.Data;
+
+namespace MovieShop.BusinessLayer.Search
+{
+    public class MovieSearchResultItem:SearchResultItem
+    {
+        [IndexField("original title")]
+        public string OriginalTitle { get; set; }
+
+        [IndexField("title")]
+        public string Title{ get; set; }
+
+        [IndexField("tagline")]
+        public string Tagline { get; set; }
+
+        [IndexField("body")]
+        public string Body { get; set; }
+
+        [IndexField("vote average")]
+        public double VoteAverage { get; set; }
+
+        [IndexField("vote count")]
+        public int VoteCount { get; set; }
+
+        [IndexField("genres")]
+        public List<ID> Genres { get; set; }
+
+        [IndexField("tmdb id")]
+        public int TmdbId { get; set; }
+
+        [IndexField("imdb id")]
+        public string ImdbId { get; set; }
+
+        [IndexField("runtime")]
+        public int Runtime { get; set; }
+
+        [IndexField("status")]
+        public string Status{ get; set; }
+
+        [IndexField("release date")]
+        public DateTime ReleaseDate { get; set; }
+
+        [IndexField("production companies")]
+        public List<ID> ProductionCompanies { get; set; }
+
+        [IndexField("production contries")]
+        public List<ID> ProductionContries { get; set; }
+    }
+}
---
+++ MovieShop/MovieShop.BusinessLayer/Search/SearchHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.ContentSearch;
+
+namespace MovieShop.BusinessLayer.Search
+{
+    public class SearchHelper
+    {
+        public static List<MovieSearchResultItem> SearchMovies()
+        {
+            var indexName = "movieshop_master";
+            using (var context = ContentSearchManager.GetIndex(indexName).CreateSearchContext())
+            {
+                var query = context.GetQueryable<MovieSearchResultItem>()
+                    .OrderByDescending(x => x.VoteAverage)
+                    .Take(10);
+                return query.ToList();
+            }
+
+        } 
+    }
+}
